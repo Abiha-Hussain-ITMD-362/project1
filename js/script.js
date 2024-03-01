7448808ca1289fa2ae0d783e534b9d2dd47802dd
ahussain33 <113541906+ahussain33@users.noreply.github.com>
2024-02-29 20:16:35 -0600

    Update script.js
    This code listens for the submit button to be pressed, then alerts the user that they are now subscribed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
+function init(){
+  var button = document.getElementById("subscribe");
+  var input = document.getElementById("input-name");
+  button.addEventListener("click", myFunction);
+ 
 
+  function myFunction() {
+    text = ('Thank you' + input.value + ', you are now Subscribed!');
+    alert(text);
+  }
+}
+window.addEventListener('load', init);
